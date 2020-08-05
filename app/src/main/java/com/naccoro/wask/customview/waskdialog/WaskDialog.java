190084a6f6b1506002dadf74ed96c3b58adc37e9
김성규 <link5658@gmail.com>
2020-08-06 02:44:38 +0900

    docs: 주석 추가

--- app/src/main/java/com/naccoro/wask/customview/waskdialog/WaskDialog.java
+++ app/src/main/java/com/naccoro/wask/customview/waskdialog/WaskDialog.java
@@ -22,6 +22,12 @@ import com.naccoro.wask.utils.MetricsUtil;
 
 import java.util.List;
 
+/**
+ * WaskDialog 클래스
+ * @author KIM SEONGGYU
+ * @version 1.0.0
+ * @since 2020.08.06
+ */
 public class WaskDialog extends DialogFragment {
 
     private static final String TAG = "WaskDialog";
@@ -54,13 +60,16 @@ public class WaskDialog extends DialogFragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.dialog_wask, container);
 
+        //속성이 올바르게 적용되었는지 확인
         contentCheck();
 
-        initView(view, inflater, container);
+        //뷰 초기화
+        initView(view, inflater);
 
         if (getDialog() != null) {
             Window window = getDialog().getWindow();
             if (window != null) {
+                //다이얼로그의 윤곽선을 둥글게 깎기
                 window.setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
                 window.requestFeature(Window.FEATURE_NO_TITLE);
             } else {
@@ -69,68 +78,87 @@ public class WaskDialog extends DialogFragment {
         } else {
             Log.e(TAG, "Dialog is null.");
         }
-        setCancelable(false);
 
         return view;
     }
 
-    private void initView(View view, LayoutInflater inflater, ViewGroup container) {
+    private void initView(View view, LayoutInflater inflater) {
         textViewDialogTitle = view.findViewById(R.id.textview_dialog_title);
         textViewDialogMessage = view.findViewById(R.id.textview_dialog_message);
         linearLayoutDialogContent = view.findViewById(R.id.linearlayout_dialog_content);
         linearLayoutDialogButtons = view.findViewById(R.id.linearlayout_dialog_buttons);
 
+        //타이틀과 메시지 초기화
         initTextView(textViewDialogTitle, title);
         initTextView(textViewDialogMessage, message);
 
+        //컨텐츠 적용
         if (contentRes != -1) {
             contentView = inflater.inflate(contentRes, linearLayoutDialogContent);
-        } else {
+        } else { //컨텐츠가 없다면 약간의 마진을 줌
             ((ConstraintLayout.LayoutParams) linearLayoutDialogContent.getLayoutParams())
                     .setMargins(0, (int) MetricsUtil.convertDpToPixel(19, getContext()), 0, 0);
         }
 
+        //세로 버튼 적용
         if (verticalButtons != null) {
             initButtons(verticalButtons, inflater, linearLayoutDialogButtons, LinearLayout.VERTICAL);
         }
 
+        //가로 버튼 적용
         if (horizontalButtons != null) {
+            //가로 버튼을 담을 하나의 LinearLayout을 생성
             LinearLayout verticalButtonGroup = new LinearLayout(getContext());
             verticalButtonGroup.setLayoutParams(new LinearLayout.LayoutParams(
                     LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
 
+            //생성한 LinearLayout에 버튼들을 추가
             initButtons(horizontalButtons, inflater, verticalButtonGroup, LinearLayout.HORIZONTAL);
+
+            //버튼이 추가된 LinearLayout을 다이얼로그에 추가
             linearLayoutDialogButtons.addView(verticalButtonGroup);
         }
     }
 
     private void initButtons(List<WaskDialogButton> buttons, LayoutInflater inflater, ViewGroup container, int orientation) {
+        //각 버튼들에 대해 로직 수행
         for (WaskDialogButton waskDialogButton : buttons) {
 
+            //버튼을 inflate하고 아직 붙이지는 않음
             View button = inflater.inflate(R.layout.item_dialog_button, container, false);
 
+            //가로 버튼이라면 weight 값을 줘 균등하게 가로 길이를 가지도록 함
             if (orientation == LinearLayout.HORIZONTAL) {
                 button.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT, 1f));
+                //첫 번째 버튼이 아니라면
                 if (waskDialogButton != buttons.get(0)) {
+                    //구분선 삽입
                     setButtonDivider(container);
                 }
             }
 
+            //버튼에 문자열 적용
             ((TextView) button.findViewById(R.id.textview_dialogbuttonitem)).setText(waskDialogButton.getText());
 
+            //버튼에 리스너 적용
             button.setOnClickListener((View v) -> waskDialogButton.getListener().onClick(this, contentView));
 
+            //버튼 추가
             container.addView(button);
         }
     }
 
     private void setButtonDivider(ViewGroup container) {
+        //뷰 하나 생성
         View divider = new View(getContext());
 
+        //간격을 1로 하여 선처럼 보이게 함
         divider.setLayoutParams(new LinearLayout.LayoutParams((int) MetricsUtil.convertDpToPixel(1f, getContext()), LinearLayout.LayoutParams.MATCH_PARENT));
 
+        //색 지정
         divider.setBackgroundResource(R.color.dividerGray);
 
+        //삽입
         container.addView(divider);
     }
 
@@ -143,6 +171,7 @@ public class WaskDialog extends DialogFragment {
     }
 
     private void contentCheck() {
+        //메시지와 컨텐츠를 함께 사용하면 예외 발생
         if (message != null && contentRes != -1) {
             throw new IllegalArgumentException("Message cannot be used with ContentRes. Please choose one of the two.");
         }
--- app/src/main/java/com/naccoro/wask/customview/waskdialog/WaskDialogBuilder.java
+++ app/src/main/java/com/naccoro/wask/customview/waskdialog/WaskDialogBuilder.java
@@ -5,6 +5,12 @@ import androidx.annotation.LayoutRes;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * WaskDialog 빌더 클래스
+ * @author KIM SEONGGYU
+ * @version 1.0.0
+ * @since 2020.08.06
+ */
 public class WaskDialogBuilder {
 
     private String title;
@@ -13,25 +19,53 @@ public class WaskDialogBuilder {
     private List<WaskDialogButton> verticalButtons;
     private List<WaskDialogButton> horizontalButtons;
 
+    /**
+     * 필요한 옵션을 모두 적용한 후 build() 하면 최종 WaskDialog 객체를 리턴.
+     * @return 옵션이 적용된 WaskDialog 객체
+     */
     public WaskDialog build() {
         return new WaskDialog(title, message, contentRes, verticalButtons, horizontalButtons);
     }
 
+    /**
+     * 다이얼로그의 타이틀을 적용
+     * @param title 적용할 타이틀 문자열
+     * @return 메서드 체이닝을 위한 자기 자신을 리턴
+     */
     public WaskDialogBuilder setTitle(String title) {
         this.title = title;
         return this;
     }
 
+    /**
+     * 다이얼로그의 메시지를 적용
+     * contentRes와 함께 적용할 경우 예외가 발생
+     * @param message 적용할 메시지 문자열
+     * @return 메서드 체이닝을 위한 자기 자신을 리턴
+     */
     public WaskDialogBuilder setMessage(String message) {
         this.message = message;
         return this;
     }
 
+    /**
+     * 다이얼로그의 컨텐츠를 적용
+     * layout의 resourse를 입력하여야 함. (ex. R.layout.item)
+     * message와 함께 적용할 경우 예외가 발생
+     * @param contentRes 적용할 레이아웃
+     * @return 메서드 체이닝을 위한 자기 자신을 리턴
+     */
     public WaskDialogBuilder setContent(@LayoutRes int contentRes) {
         this.contentRes = contentRes;
         return this;
     }
 
+    /**
+     * 세로로 배열되는 버튼을 추가
+     * @param buttonText 버튼에 들어갈 문자열
+     * @param listener 버튼을 눌렀을 때 실행될 리스너
+     * @return 메서드 체이닝을 위한 자기 자신을 리턴
+     */
     public WaskDialogBuilder addVerticalButton(String buttonText, WaskDialog.OnClickListener listener) {
         if (verticalButtons == null) {
             verticalButtons = new ArrayList<>();
@@ -40,11 +74,22 @@ public class WaskDialogBuilder {
         return this;
     }
 
+    /**
+     * 세로로 배열되는 버튼을 직접 추가
+     * @param button 버튼 문구와 리스너를 담은 WaskDialogButton 객체
+     * @return 메서드 체이닝을 위한 자기 자신을 리턴
+     */
     public WaskDialogBuilder addVerticalButton(WaskDialogButton button) {
         verticalButtons.add(button);
         return this;
     }
 
+    /**
+     * 가로로 배열되는 버튼을 추가
+     * @param buttonText 버튼에 들어갈 문자열
+     * @param listener 버튼을 눌렀을 때 실행될 리스너
+     * @return 메서드 체이닝을 위한 자기 자신을 리턴
+     */
     public WaskDialogBuilder addHorizontalButton(String buttonText, WaskDialog.OnClickListener listener) {
         if (horizontalButtons == null) {
             horizontalButtons = new ArrayList<>();
@@ -53,6 +98,11 @@ public class WaskDialogBuilder {
         return this;
     }
 
+    /**
+     * 가로로 배열되는 버튼을 직접 추가
+     * @param button 버튼 문구와 리스너를 담은 WaskDialogButton 객체
+     * @return 메서드 체이닝을 위한 자기 자신을 리턴
+     */
     public WaskDialogBuilder addHorizontalButton(WaskDialogButton button) {
         horizontalButtons.add(button);
         return this;
