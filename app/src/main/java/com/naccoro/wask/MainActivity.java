ab420cdb86b41f9d9519179063a17350b95c902f
euzl <euzl@khu.ac.kr>
2020-08-07 01:23:31 +0900

    feat: 캘린더 버튼 추가
    Issue: #6

--- app/src/main/java/com/naccoro/wask/MainActivity.java
+++ app/src/main/java/com/naccoro/wask/MainActivity.java
@@ -11,6 +11,7 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     ImageView settingButton;
+    ImageView calendarButton;
     ImageView emotionImageView;
     TextView cardMessageTextView;
     TextView usePeriodTextView;
@@ -26,12 +27,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private void initView() {
         settingButton = findViewById(R.id.imageview_setting);
+        calendarButton = findViewById(R.id.imageview_calendar);
         emotionImageView = findViewById(R.id.imageview_emotion);
         cardMessageTextView = findViewById(R.id.textview_card_message);
         usePeriodTextView = findViewById(R.id.textview_use_period);
         changeButton = findViewById(R.id.button_change);
 
         settingButton.setOnClickListener(this);
+        calendarButton.setOnClickListener(this);
         changeButton.setOnClickListener(this);
     }
 
@@ -40,6 +43,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         switch(view.getId()) {
             case R.id.imageview_setting:
                 // 셋팅 액티비티로 이동
+                break;
+            case R.id.imageview_calendar:
+                // 캘린더 액티비티로 이동
                 break;
             case R.id.button_change:
                 // '교체하기' 버튼을 눌렀을 때
