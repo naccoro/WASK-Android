1eb75b990c9c0c9ec453d39e578bfc06ebc51b1d
jaeryo2357 <jaeryo2357@naver.com>
2020-08-11 17:48:40 +0900

    feat: 마스크 사용일자에 따른 메인화면 변화
    Resolves: #3

--- app/src/main/java/com/naccoro/wask/main/MainContract.java
+++ app/src/main/java/com/naccoro/wask/main/MainContract.java
@@ -4,12 +4,25 @@ public interface MainContract {
 
     interface View {
         void showSettingView();
+
         void showCalendarView();
+
+        void showGoodMainView();
+
+        void showBadMainView();
+
+        void setPeriodTextValue(int period);
     }
 
     interface Presenter {
+
+        void start();
+
         void clickSettingButton();
+
         void clickCalendarButton();
+
+        void changeMask();
     }
 
 }
--- app/src/main/java/com/naccoro/wask/main/MainPresenter.java
+++ app/src/main/java/com/naccoro/wask/main/MainPresenter.java
@@ -7,6 +7,31 @@ public class MainPresenter implements MainContract.Presenter {
         this.mainView = mainView;
     }
 
+    /**
+     * MainView에서 보여줘야 할 Data를 가져오는 함수
+     */
+    @Override
+    public void start() {
+        int period = getMaskPeriod();
+        mainView.setPeriodTextValue(period);
+
+        if (period > 1) {
+            mainView.showBadMainView();
+        } else {
+            mainView.showGoodMainView();
+        }
+    }
+
+    /**
+     * WaskDatabase에서 현재 마스크 교체 상태를 가져오는 함수
+     *
+     * @return [오늘 날짜 - 마지막 교체 일자]
+     */
+    private int getMaskPeriod() {
+        //TODO: WaskDatabase에서 교체일자를 비교하여 현재 상태를 가져온다.
+        return 3;
+    }
+
     @Override
     public void clickSettingButton() {
         mainView.showSettingView();
@@ -15,5 +40,14 @@ public class MainPresenter implements MainContract.Presenter {
     @Override
     public void clickCalendarButton() {
         mainView.showCalendarView();
+    }
+
+    /**
+     * 사용자가 교체하기 버튼을 누를 경우 호출되는 함수
+     */
+    @Override
+    public void changeMask() {
+        //TODO: WaskDatabase에서 교체일자로 오늘 날짜를 넣는다.
+        start();
     }
 }
