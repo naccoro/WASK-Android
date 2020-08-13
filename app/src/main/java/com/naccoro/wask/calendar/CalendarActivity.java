9d2432b97578d29ab286ae8f97fd83b6b331b0ae
euzl <euzl@khu.ac.kr>
2020-08-13 19:46:37 +0900

    feat: calendar
    살려주세요...

--- app/src/main/java/com/naccoro/wask/calendar/CalendarActivity.java
+++ app/src/main/java/com/naccoro/wask/calendar/CalendarActivity.java
@@ -1,17 +1,28 @@
 package com.naccoro.wask.calendar;
 
+import android.icu.util.Calendar;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 
 import com.naccoro.wask.R;
 
+import java.util.ArrayList;
+import java.util.GregorianCalendar;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class CalendarActivity extends AppCompatActivity
         implements View.OnClickListener, CalendarContract.View {
 
     ImageView backButton;
+    RecyclerView recyclerView;
+    CalendarAdapter calendarAdapter;
+    GridLayoutManager gridLayoutManager;
+
+    ArrayList<CalendarItem> dateList;
 
     private CalendarPresenter presenter;
 
@@ -23,12 +34,23 @@ public class CalendarActivity extends AppCompatActivity
         presenter = new CalendarPresenter(this);
 
         initView();
+
+        setCalendarList();
+        calendarAdapter.notifyDataSetChanged();
     }
 
     private void initView() {
         backButton = findViewById(R.id.imageview_back);
 
         backButton.setOnClickListener(this);
+
+        // 리사이클러뷰 초기화
+        recyclerView = findViewById(R.id.recyclerview_calender);
+        calendarAdapter = new CalendarAdapter(dateList);
+        gridLayoutManager = new GridLayoutManager(this, 7);
+
+        recyclerView.setAdapter(calendarAdapter);
+        recyclerView.setLayoutManager(gridLayoutManager);
     }
 
     @Override
@@ -49,5 +71,33 @@ public class CalendarActivity extends AppCompatActivity
     public void finish() {
         super.finish();
         overridePendingTransition(R.anim.slide_activity_fadein, R.anim.slide_activity_fadeout);
+    }
+
+    public void setCalendarList() {
+        GregorianCalendar cal = new GregorianCalendar();
+        dateList = new ArrayList<CalendarItem>();
+        for (int i = 0; i < 1; i++) {
+            try {
+                GregorianCalendar calendar = new GregorianCalendar(cal.get(Calendar.YEAR), cal.get(Calendar.MONTH) + i, 1, 0, 0, 0);
+
+                int startDayOfWeek = calendar.get(Calendar.DAY_OF_WEEK) - 1; // 해당 월에 시작하는 요일
+                int lasyDayOfMonth = calendar.getActualMaximum(Calendar.DAY_OF_MONTH); // 해당 월에 마지막 요일
+
+                // 지난 달 저장
+//                for (int j = 0; j < startDayOfWeek; j++) {
+//                    dateList.add(new CalendarItem(false, false, ))
+//                }
+
+                // 이번 달 저장
+                for (int j = 1; j <= lasyDayOfMonth; j++) {
+                    GregorianCalendar item = new GregorianCalendar(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), j);
+                    dateList.add(new CalendarItem(false, false, item));
+                }
+
+                // 다음 달 저장
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/naccoro/wask/calendar/CalendarAdapter.java
@@ -0,0 +1,97 @@
+package com.naccoro.wask.calendar;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.naccoro.wask.R;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class CalendarAdapter extends RecyclerView.Adapter<CalendarAdapter.CalendarViewHolder> {
+
+    private ArrayList<CalendarItem> calendarList;
+
+    public CalendarAdapter(ArrayList<CalendarItem> calendarList) {
+        this.calendarList = calendarList;
+    }
+
+    public void setCalendarList(ArrayList<CalendarItem> calendarList) {
+        this.calendarList = calendarList;
+        notifyDataSetChanged();
+    }
+
+    /**
+     * 아이템 뷰를 넣기 위한 view holder 생성
+     *
+     * @param parent
+     * @param viewType
+     * @return
+     */
+    @NonNull
+    @Override
+    public CalendarAdapter.CalendarViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Context context = parent.getContext() ;
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) ;
+
+        View view = inflater.inflate(R.layout.calendar_item, parent, false) ;
+        CalendarAdapter.CalendarViewHolder viewHolder = new CalendarAdapter.CalendarViewHolder(view) ;
+
+        return viewHolder ;
+    }
+
+    /**
+     * position에 해당하는 data를 view holder에 넣기
+     *
+     * @param calendarViewHolder
+     * @param position
+     */
+    @Override
+    public void onBindViewHolder(@NonNull CalendarViewHolder calendarViewHolder, int position) {
+        CalendarItem item = calendarList.get(position);
+        calendarViewHolder.dateTextView.setText(item.getDate().get(Calendar.DAY_OF_MONTH));
+    }
+
+    /**
+     * item 몇 개 생성되는지
+     * 제대로 생성되지 않으면 오류가 난다고 합니다.
+     *
+     * @return item 개수 반환
+     */
+    @Override
+    public int getItemCount() {
+        if (calendarList != null) {
+            return calendarList.size();
+        }
+        return 0;
+    }
+
+    /**
+     * calendar view holder
+     */
+    public class CalendarViewHolder extends RecyclerView.ViewHolder {
+        private TextView dateTextView;
+        private ImageView dateBackgroundImageView;
+        private ImageView changeImageView;
+
+        public CalendarViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            initView(itemView);
+        }
+
+        public void initView(View v) {
+            dateTextView = v.findViewById(R.id.textView_date);
+            dateBackgroundImageView = v.findViewById(R.id.imageView_date_background);
+            changeImageView = v.findViewById(R.id.imageView_change);
+        }
+
+    }
+}
